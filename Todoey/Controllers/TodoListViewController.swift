90cc46c37aa06e175c144f8d6fb51fcfde435478
Manon huet <manonhuet@node-1w7jra9317kvt0fou569p9vs0.ipv6.telus.net>
2018-07-21 13:34:48 -0400

    Alléger le code en une seul ligne(condition if bool)

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,17 +11,30 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
   
-    var itemArray =  ["Find Milk", "Buy Eggos", "Destory Demogorgon"]
+    var itemArray =  [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
         // TableView delegate
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-    }
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//           itemArray = items
+//    }
 }
     //Mark - Méthode de source de données(Écrire -> TableView) pour spécifier ce que les cellules doivent afficher et combien de lignes.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,35 +42,44 @@ class TodoListViewController: UITableViewController {
         return itemArray.count
     }
 
-    //insert le chemin location Table view (indexPath)
+    //MARK - insert le chemin location Table view (indexPath)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         
         //Créer une cellule, qui contient le nom de notre cellule."ToDoItemCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        //Envoyer l'étiquette de texte dans chaque cellule unique.
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
+        //Envoyer l'étiquette de texte dans chaque cellule unique.
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+       /* if item.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }*/
         //Retourner à la Table View comme une route
         return cell 
     }
     
-    // TableView delegate méthode
+    // MARK - TableView delegate méthode
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //Imprimer index(numéro) "print(indexPath.row)"  dans la console de débogage lorsque nous sélectionnons une des cellules. Imprimer le texte "print(itemArray[indexPath.row])
-        print(itemArray[indexPath.row])
         
-        //Méthode enleve les éléments sélectionnés, Si "if" il y a un accessoire qui est cocher, peut décocher et la coche disparait
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            // si la cellule est cocher, enlever la coche
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
+        // Remplace le code en dessous.
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+      /*  if itemArray[indexPath.row].done == false {
+            itemArray[indexPath].done = true
         } else {
-            
-            //sinon mettre une coche
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        }*/
+        
+        tableView.reloadData()
         
         //La cellule ne reste pas sélectionnés en gris, elle redevient blanche.
         tableView.deselectRow(at: indexPath, animated: true)
@@ -77,8 +99,11 @@ class TodoListViewController: UITableViewController {
         // Créer une action
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
+            let newItem = Item()
+            newItem.title = textField.text!
+            
             //Action lorsque l'utilisateur clique sur le bouton pour ajouter un item(+), il ajoute dans le tableau.
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Manon Huet on 18-07-21.
+//  Copyright © 2018 Manon Huet. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
